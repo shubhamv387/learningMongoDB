1e96a883ce6fbe0ed6c3e7cdf843b6b64a3f9fc3
Shubham Kumar Vishwakarma <shubhamv387@gmail.com>
2023-09-14 13:55:35 +0530

    added User model

--- models/user.js
+++ models/user.js
@@ -1,16 +1,30 @@
-const Sequelize = require('sequelize');
+const mongodb = require("mongodb");
+const getdb = require("../util/database").getdb;
+class User {
+  constructor(username, email) {
+    this.name = username;
+    this.email = email;
+  }
 
-const sequelize = require('../util/database');
+  save() {
+    const db = getdb();
+    db.collection("users")
+      .insertOne(this)
+      .then()
+      .catch((err) => console.log(err));
+  }
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+  static findById(userId) {
+    const db = getdb();
+    return db
+      .collection("users")
+      .findOne({ _id: new mongodb.ObjectId(userId) })
+      .then((user) => {
+        console.log(user);
+        return user;
+      })
+      .catch((err) => console.log(err));
+  }
+}
 
 module.exports = User;
